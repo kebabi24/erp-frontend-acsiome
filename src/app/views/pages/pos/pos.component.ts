f1734ecad7724e0db4240f3654d1780a857a3552
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2022-12-12 19:38:39 +0100

    pos

--- src/app/views/pages/pos/pos.component.ts
+++ src/app/views/pages/pos/pos.component.ts
@@ -49,24 +49,24 @@ import {
   OnEventArgs,
 } from "angular-slickgrid";
 
-import {
-  PosPrinter,
-  PosPrintData,
-  PosPrintOptions,
-} from "electron-pos-printer";
-import * as path from "path";
+// import {
+//   PosPrinter,
+//   PosPrintData,
+//   PosPrintOptions,
+// } from "electron-pos-printer";
+// import * as path from "path";
 
 import { environment } from "../../../../environments/environment";
 const API_URL = environment.apiUrl + "/codes";
-const options: PosPrintOptions = {
-  preview: false,
-  margin: "0 0 0 0",
-  copies: 1,
-  printerName: "XP-80C",
-  timeOutPerLine: 400,
-  pageSize: "80mm", // page size
-  boolean: false,
-};
+// const options: PosPrintOptions = {
+//   preview: false,
+//   margin: "0 0 0 0",
+//   copies: 1,
+//   printerName: "XP-80C",
+//   timeOutPerLine: 400,
+//   pageSize: "80mm", // page size
+//   boolean: false,
+// };
 @Component({
   selector: "kt-pos",
   templateUrl: "./pos.component.html",
@@ -972,83 +972,83 @@ export class PosComponent implements OnInit {
       site_loc: this.currentTable ? this.currentTable : null,
       from: "BOUTIQUE",
     };
-    const data: PosPrintData[] = [
-      {
-        type: "image",
-        url: "https://randomuser.me/api/portraits/men/43.jpg", // file path
-        position: "center", // position of image: 'left' | 'center' | 'right'
-        width: "160px", // width of image in px; default: auto
-        height: "60px", // width of image in px; default: 50 or '50px'
-      },
-      {
-        type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-        value: "SAMPLE HEADING",
-        style: { fontWeight: "700", textAlign: "center", fontSize: "24px" },
-      },
-      {
-        type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table'
-        value: "Secondary text",
-        style: {
-          textDecoration: "underline",
-          fontSize: "10px",
-          textAlign: "center",
-          color: "red",
-        },
-      },
-      {
-        type: "barCode",
-        value: "023456789010",
-        height: "40px", // height of barcode, applicable only to bar and QR codes
-        width: "2px", // width of barcode, applicable only to bar and QR codes
-        displayValue: true, // Display value below barcode
-        fontsize: 12,
-      },
-      {
-        type: "qrCode",
-        value: "https://github.com/Hubertformin/electron-pos-printer",
-        height: "55px",
-        width: "55px",
-        style: { margin: "10 20px 20 20px" },
-      },
-      {
-        type: "table",
-        // style the table
-        style: { border: "1px solid #ddd" },
-        // list of the columns to be rendered in the table header
-        tableHeader: ["Animal", "Age"],
-        // multi dimensional array depicting the rows and columns of the table body
-        tableBody: [
-          ["Cat", "2"],
-          ["Dog", "4"],
-          ["Horse", "12"],
-          ["Pig", "4"],
-        ],
-        // list of columns to be rendered in the table footer
-        tableFooter: ["Animal", "Age"],
-        // custom style for the table header
-        tableHeaderStyle: { backgroundColor: "#000", color: "white" },
-        // custom style for the table body
-        tableBodyStyle: { border: "0.5px solid #ddd" },
-        // custom style for the table footer
-        tableFooterStyle: { backgroundColor: "#000", color: "white" },
-      },
-      {
-        type: "table",
-        style: { border: "1px solid #ddd" }, // style the table
-        // list of the columns to be rendered in the table header
-        tableHeader: [{ type: "text", value: "People" }],
-        // multi-dimensional array depicting the rows and columns of the table body
+    // const data: PosPrintData[] = [
+    //   {
+    //     type: "image",
+    //     url: "https://randomuser.me/api/portraits/men/43.jpg", // file path
+    //     position: "center", // position of image: 'left' | 'center' | 'right'
+    //     width: "160px", // width of image in px; default: auto
+    //     height: "60px", // width of image in px; default: 50 or '50px'
+    //   },
+    //   {
+    //     type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
+    //     value: "SAMPLE HEADING",
+    //     style: { fontWeight: "700", textAlign: "center", fontSize: "24px" },
+    //   },
+    //   {
+    //     type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table'
+    //     value: "Secondary text",
+    //     style: {
+    //       textDecoration: "underline",
+    //       fontSize: "10px",
+    //       textAlign: "center",
+    //       color: "red",
+    //     },
+    //   },
+    //   {
+    //     type: "barCode",
+    //     value: "023456789010",
+    //     height: "40px", // height of barcode, applicable only to bar and QR codes
+    //     width: "2px", // width of barcode, applicable only to bar and QR codes
+    //     displayValue: true, // Display value below barcode
+    //     fontsize: 12,
+    //   },
+    //   {
+    //     type: "qrCode",
+    //     value: "https://github.com/Hubertformin/electron-pos-printer",
+    //     height: "55px",
+    //     width: "55px",
+    //     style: { margin: "10 20px 20 20px" },
+    //   },
+    //   {
+    //     type: "table",
+    //     // style the table
+    //     style: { border: "1px solid #ddd" },
+    //     // list of the columns to be rendered in the table header
+    //     tableHeader: ["Animal", "Age"],
+    //     // multi dimensional array depicting the rows and columns of the table body
+    //     tableBody: [
+    //       ["Cat", "2"],
+    //       ["Dog", "4"],
+    //       ["Horse", "12"],
+    //       ["Pig", "4"],
+    //     ],
+    //     // list of columns to be rendered in the table footer
+    //     tableFooter: ["Animal", "Age"],
+    //     // custom style for the table header
+    //     tableHeaderStyle: { backgroundColor: "#000", color: "white" },
+    //     // custom style for the table body
+    //     tableBodyStyle: { border: "0.5px solid #ddd" },
+    //     // custom style for the table footer
+    //     tableFooterStyle: { backgroundColor: "#000", color: "white" },
+    //   },
+    //   {
+    //     type: "table",
+    //     style: { border: "1px solid #ddd" }, // style the table
+    //     // list of the columns to be rendered in the table header
+    //     tableHeader: [{ type: "text", value: "People" }],
+    //     // multi-dimensional array depicting the rows and columns of the table body
 
-        // list of columns to be rendered in the table footer
+    //     // list of columns to be rendered in the table footer
 
-        // custom style for the table header
-        tableHeaderStyle: { backgroundColor: "red", color: "white" },
-        // custom style for the table body
-        tableBodyStyle: { border: "0.5px solid #ddd" },
-        // custom style for the table footer
-        tableFooterStyle: { backgroundColor: "#000", color: "white" },
-      },
-    ];
+    //     // custom style for the table header
+    //     tableHeaderStyle: { backgroundColor: "red", color: "white" },
+    //     // custom style for the table body
+    //     tableBodyStyle: { border: "0.5px solid #ddd" },
+    //     // custom style for the table footer
+    //     tableFooterStyle: { backgroundColor: "#000", color: "white" },
+    //   },
+    // ];
     // PosPrinter.print(data, options)
     //   .then(console.log)
     //   .catch((error) => {
