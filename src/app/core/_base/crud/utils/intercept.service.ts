7287a2019368d71f40dff09a83bcdee4ae3b42ec
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2023-01-23 00:59:53 +0100

    newserver

--- src/app/core/_base/crud/utils/intercept.service.ts
+++ src/app/core/_base/crud/utils/intercept.service.ts
@@ -1,9 +1,15 @@
 // Angular
-import { Injectable } from '@angular/core';
-import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import {
+  HttpEvent,
+  HttpInterceptor,
+  HttpHandler,
+  HttpRequest,
+  HttpResponse,
+} from "@angular/common/http";
 // RxJS
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 /**
  * More information there => https://medium.com/@MetonymyQT/angular-http-interceptors-what-are-they-and-how-to-use-them-52e060321088
  */
@@ -16,32 +22,28 @@ export class InterceptService implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     // tslint:disable-next-line:no-debugger
     // modify request
-    const user = JSON.parse(localStorage.getItem('user'))
-    console.log(user)
+    const user = JSON.parse(localStorage.getItem("user"));
+    console.log(user);
     request = request.clone({
-    	setHeaders: {
-       USER_CODE:user ? user.usrd_code : '',
-       USER_DOMAIN:user ? user.usrd_domain : '',
-
-        
-    	}
+      setHeaders: {
+        USER_CODE: user ? user.usrd_code : "",
+      },
     });
-    
+
     // console.log('----request----');
     // console.log(request);
-   // console.log('--- end of request---');
+    // console.log('--- end of request---');
 
-   
     return next.handle(request).pipe(
       tap(
-        event => {
+        (event) => {
           if (event instanceof HttpResponse) {
-     //       console.log('all looks good');
+            //       console.log('all looks good');
             // http response status code
             // console.log(event.status);
           }
         },
-        error => {
+        (error) => {
           // http response status code
           // console.log('----response----');
           // console.error('status code:');
