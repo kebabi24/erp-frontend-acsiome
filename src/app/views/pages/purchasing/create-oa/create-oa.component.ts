3d67f541208fcbe9d430925275cb9372f0841d59
kamelneo <kamel.bensaoud@gmail.com>
2023-01-26 21:41:43 +0100

    create-oa

--- src/app/views/pages/purchasing/create-oa/create-oa.component.ts
+++ src/app/views/pages/purchasing/create-oa/create-oa.component.ts
@@ -357,6 +357,7 @@ console.log(resp.data)
       editable: true,
       enableColumnPicker: true,
       enableCellNavigation: true,
+      enableAutoResize: true,
       enableRowSelection: true,
       autoHeight: true,
       formatterOptions: {
@@ -386,13 +387,14 @@ console.log(resp.data)
   }
 
   addNewItem() {
+    const controls = this.poForm.controls
  this.dataset = []
     
  for(let data of this.mvdataset) {
    console.log(data)
  }
  console.log("allllo")
- var site = this.user.usrd_site
+ var site = controls.site.value 
  var detail = this.mvdataset
       this.psService.getBySpec({site,detail}).subscribe((response: any)=>{
         
@@ -470,9 +472,9 @@ console.log(resp.data)
         
       },*/
       {
-        id: "pt_part",
+        id: "part",
         name: "Code Produit",
-        field: "pt_part",
+        field: "part",
         sortable: true,
         width: 50,
         filterable: false,
@@ -482,9 +484,9 @@ console.log(resp.data)
         },
       },
       {
-        id: "pt_desc1",
+        id: "desc1",
         name: "Description",
-        field: "pt_desc1",
+        field: "desc1",
         sortable: true,
         width: 80,
         filterable: false,
@@ -541,7 +543,7 @@ console.log(resp.data)
       enableColumnPicker: true,
       enableCellNavigation: true,
       enableRowSelection: true,
-      enableAutoResize: false,
+      enableAutoResize: true,
       autoHeight: true,
       formatterOptions: {
         
@@ -561,9 +563,13 @@ console.log(resp.data)
     
     console.log(this.user)
     const controls = this.poForm.controls
-    const date = new Date(`${controls.calc_date.value.year}/${controls.calc_date.value.month}/${controls.calc_date.value.day}`)
+    
+    const date = controls.calc_date.value
+    ? `${controls.calc_date.value.year}/${controls.calc_date.value.month}/${controls.calc_date.value.day}`
+    : null;
+   // const date = new Date(`${controls.calc_date.value.year}/${controls.calc_date.value.month}/${controls.calc_date.value.day}`)
     console.log(date,controls.calc_date.value)
-    this.posCategoryService.getSumeQtyPs({usrd_site: this.user.usrd_site, created_date: date}).subscribe(
+    this.posCategoryService.getSumeQtyPs({usrd_site: controls.site.value, created_date: date}).subscribe(
       (response: any) => {   
         this.mvdataset = response.data
        console.log(this.mvdataset)
@@ -580,9 +586,10 @@ console.log(resp.data)
     this.mvdataset = []
     this.dataset = []
     const controls = this.poForm.controls
-    const date = new Date(`${controls.calc_date.value.year}/${controls.calc_date.value.month}/${controls.calc_date.value.day}`)
-    console.log(date,controls.calc_date.value)
-    this.posCategoryService.getSumeQtyPs({usrd_site: this.user.usrd_site, created_date: date}).subscribe(
+    const date = controls.calc_date.value
+    ? `${controls.calc_date.value.year}/${controls.calc_date.value.month}/${controls.calc_date.value.day}`
+    : null;    console.log(date,controls.calc_date.value)
+    this.posCategoryService.getSumeQtyPs({usrd_site: controls.site.value, created_date: date}).subscribe(
       (response: any) => {   
         this.mvdataset = response.data
        console.log(this.mvdataset)
@@ -604,6 +611,7 @@ console.log(resp.data)
     this.poForm = this.poFB.group({
      // po_category: [{value: this.purchaseOrder.po_category, disabled:true}, Validators.required],
       qty: [70],
+      site:[this.user.usrd_site,Validators.required],
       calc_date: [{
         year:date.getFullYear(),
         month: date.getMonth()+1,
@@ -729,6 +737,128 @@ console.log(resp.data)
   // add new Item to Datatable
   onAlertClose($event) {
     this.hasFormErrors = false;
+  }
+
+  onChangesite() {
+    const controls = this.poForm.controls;
+    const si_site = controls.site.value;
+    
+    this.siteService.getByOne({ si_site }).subscribe(
+      (res: any) => {
+  
+        if (!res.data) {
+  
+            alert("Site n'existe pas  ")
+            controls.site.setValue(null);
+            document.getElementById("site").focus();
+          } else {
+           if( this.user.usrd_site != "*" && si_site != this.user.usrd_site){
+            alert("Site n'est pas autorisé pour cet utilisateur ")
+            controls.site.setValue(null);
+            document.getElementById("site").focus();
+             
+
+
+           } 
+          }
+      
+      });
+  }
+  
+ 
+  handleSelectedRowsChangedsite(e, args) {
+    const controls = this.poForm.controls;
+      if (Array.isArray(args.rows) && this.gridObjsite) {
+      args.rows.map((idx) => {
+        const item = this.gridObjsite.getDataItem(idx);
+        console.log(item);
+        
+       controls.site.setValue(item.si_site);
+        
+    
+     
+  });
+
+    }
+  }
+  angularGridReadysite(angularGrid: AngularGridInstance) {
+    this.angularGridsite = angularGrid;
+    this.gridObjsite = (angularGrid && angularGrid.slickGrid) || {};
+  }
+
+  prepareGridsite() {
+    this.columnDefinitionssite = [
+      
+      {
+        id: "id",
+        name: "id",
+        field: "id",
+        sortable: true,
+        minWidth: 80,
+        maxWidth: 80,
+      },
+      {
+        id: "si_site",
+        name: "Site",
+        field: "si_site",
+        sortable: true,
+        filterable: true,
+        type: FieldType.string,
+      },
+      {
+        id: "si_desc",
+        name: "Designation",
+        field: "si_desc",
+        sortable: true,
+        filterable: true,
+        type: FieldType.string,
+      },
+    ];
+
+    this.gridOptionssite = {
+        enableSorting: true,
+        enableCellNavigation: true,
+        enableExcelCopyBuffer: true,
+        enableFiltering: true,
+        autoEdit: false,
+        autoHeight: false,
+        frozenColumn: 0,
+        frozenBottom: true,
+        enableRowSelection: true,
+        enableCheckboxSelector: true,
+        checkboxSelector: {
+          // optionally change the column index position of the icon (defaults to 0)
+          // columnIndexPosition: 1,
+  
+          // remove the unnecessary "Select All" checkbox in header when in single selection mode
+          hideSelectAllCheckbox: true,
+  
+          // you can override the logic for showing (or not) the expand icon
+          // for example, display the expand icon only on every 2nd row
+          // selectableOverride: (row: number, dataContext: any, grid: any) => (dataContext.id % 2 === 1)
+        },
+        multiSelect: false,
+        rowSelectionOptions: {
+          // True (Single Selection), False (Multiple Selections)
+          selectActiveRow: true,
+        },
+      };
+    // fill the dataset with your data
+    if(this.user.usrd_site == "*") {
+    this.siteService
+      .getAll()
+      .subscribe((response: any) => (this.datasite = response.data));
+    }
+    else {
+      this.siteService
+      .getBy({si_site : this.user.usrd_site})
+      .subscribe((response: any) => (this.datasite = response.data));
+  
+    }
+  }
+  opensite(contentsite) {
+    this.prepareGridsite();
+    this.modalService.open(contentsite, { size: "lg" });
   }
 
 }
