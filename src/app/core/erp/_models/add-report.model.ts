4c3a30e55b60e2c1f7f0333027ce12eadaf5f84b
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2023-07-26 16:13:15 +0100

    invoices

--- src/app/core/erp/_models/add-report.model.ts
+++ src/app/core/erp/_models/add-report.model.ts
@@ -1,19 +1,18 @@
+import { BaseModel } from "./base.model";
 
-import {BaseModel} from './base.model'
-
-export class AddReport extends BaseModel{
-
-
-
- id: Number
-pmr_pm_code: String
-pmr_site: String   
-pmr_inst: String
-pmr_task: String
-pmr_task_status: String
-pmr_internal: Boolean
-pmr_employe: String
-pmr_close: Boolean
-pmr_domain:  String
-
+export class AddReport extends BaseModel {
+  id: Number;
+  pmr_pm_code: String;
+  pmr_site: String;
+  pmr_inst: String;
+  pmr_task: String;
+  pmr_task_status: String;
+  pmr_internal: Boolean;
+  pmr_demobilisation: Boolean;
+  pmr_mobilisation: Boolean;
+  pmr_stndby: Boolean;
+  pmr_separe: Boolean;
+  pmr_employe: String;
+  pmr_close: Boolean;
+  pmr_domain: String;
 }
