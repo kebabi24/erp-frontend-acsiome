f1548d20306f35a9e4e3e7fa5ee677dc6b5bb9f4
Mohamed Amine <mohamedamine.euldji@acsiome.tech>
2023-07-29 17:21:18 +0100

    amine_29_7_2023

--- src/app/views/pages/promotion/promo-add/promo-add.component.ts
+++ src/app/views/pages/promotion/promo-add/promo-add.component.ts
@@ -77,12 +77,28 @@ export class PromoAddComponent implements OnInit {
   popsArticle : []
 
   gridOptions6: GridOption = {}
-    columnDefinitions6: Column[] = []
-    gridObj6: any
-    angularGrid6: AngularGridInstance
-    dataView6: any
-    gridService6: GridService
-    sites : []
+  columnDefinitions6: Column[] = []
+  gridObj6: any
+  angularGrid6: AngularGridInstance
+  dataView6: any
+  gridService6: GridService
+  sites : []
+
+  gridOptions2: GridOption = {}
+  columnDefinitions2: Column[] = []
+  gridObj2: any
+  angularGrid2: AngularGridInstance
+  dataView2: any
+  gridService2: GridService
+  popsClient : []
+
+  gridOptions4: GridOption = {}
+  columnDefinitions4: Column[] = []
+  gridObj4: any
+  angularGrid4: AngularGridInstance
+  dataView4: any
+  gridService4: GridService
+  advantages : []
 
 
   constructor(
@@ -105,6 +121,8 @@ export class PromoAddComponent implements OnInit {
     this.loadingSubject.next(false)
     this.getPopsArticle()
     this.getSites()
+    this.getAdvantages()
+    this.getPopsClient()
     this.createPopulationForm()
     this.init()
   }
@@ -148,6 +166,7 @@ export class PromoAddComponent implements OnInit {
       condition : [ '', Validators.required],
       combine  : [ '', Validators.required],
       adv_code  : [ '', Validators.required],
+      rank:[ '', Validators.required],
       
    })
      
@@ -155,31 +174,38 @@ export class PromoAddComponent implements OnInit {
 
   onChangeCode() {
     const controls = this.populationForm.controls
-    const code_adv = controls.code_adv.value
-    if(code_adv === "") { 
+    const promo_code = controls.promo_code.value
+    if(promo_code === "") { 
       this.cantSearch = true 
     }
 
-    controls.type_adv.enable()
-    controls.amount_perc.enable()
+    
 
-    //  this.promotionService.createPopulationAarticle(population_code).subscribe(
-    //      (res: any) => {
-    //        if (res.data) {
-    //          alert("Ce code de population exist déja")
-    //          document.getElementById("code").focus(); 
-    //          controls.desc_population.disable()
-    //          controls.selector.disable()
-    //          controls.rank.disable()     
-    //        } else { 
-    //          this.isExist = true
-    //          this.cantSearch = false
-    //          controls.desc_population.disable()
-    //          controls.selector.disable()
-    //          controls.rank.disable()      
-    //      }
+      this.promotionService.getPromo(promo_code).subscribe(
+          (res: any) => {
+            if (res.data) {
+              alert("Ce code de population exist déja")
+              document.getElementById("code").focus(); 
+              controls.amount_perc.disable()
+              controls.type_adv.disable()     
+              controls.pop_a_code.disable()     
+              controls.pop_c_code.disable()     
+              controls.condition.disable()     
+              controls.combine.disable()     
+              controls.rank.disable()     
+            } else { 
+              this.isExist = true
+              this.cantSearch = false
+              controls.amount_perc.enable()
+              controls.type_adv.enable()     
+              controls.pop_a_code.enable()     
+              controls.pop_c_code.enable()     
+              controls.condition.enable()     
+              controls.combine.enable()     
+              controls.rank.enable()     
+          }
                
-    //  })
+      })
   } 
 
   
@@ -207,44 +233,50 @@ export class PromoAddComponent implements OnInit {
          return
        }
      
-    const adv_code = controls.code_adv.value
-    const type_adv = controls.type_adv.value
-    const amount_perc = controls.amount_perc.value
-
+    const promo_code = controls.promo_code.value
+    const pop_a_code = controls.pop_a_code.value
+    const pop_c_code = controls.pop_c_code.value
+    const site = controls.site.value
+    const start_date = controls.start_date.value
+    const end_date = controls.end_date.value
+    const condition = controls.condition.value
+    const combine = controls.combine.value
+    const adv_code = controls.adv_code.value
+    const rank = controls.rank.value
     
+    let promo = {
+      promo_code,pop_a_code,pop_c_code,site,condition,combine,adv_code,rank,
+      start_date : start_date.year + '-' +  start_date.month + '-' + start_date.day ,
+      end_date : end_date.year + '-' +  end_date.month + '-' + end_date.day 
+    }
 
-    
+      this.promotionService
+        .createPromotion(promo)
 
-    
-    //  this.promotionService
-    //    .createAdvantage(advantage)
-
-    //    .subscribe(
-    //      (res: any) => {
-    //        console.log(res);
-    //        this.prepareGrid()
-    //        this.createPopulationForm()
-    //      },
-    //      (err) =>
-    //        this.layoutUtilsService.showActionNotification(
-    //          "Erreur lors de l'ajout de la population",
-    //          MessageType.Create,
-    //          10000,
-    //          true,
-    //          true
-    //        ),
-    //      () => {
-    //        this.createPopulationForm()
-    //        this.layoutUtilsService.showActionNotification(
-    //          "Population ajoutée avec succès",
-    //          MessageType.Create,
-    //          10000,
-    //          true,
-    //          true
-    //        );
-    //        // this.router.navigateByUrl("/auth/new-customer/");
-    //      }
-    //    );
+        .subscribe(
+          (res: any) => {
+            this.createPopulationForm()
+          },
+          (err) =>
+            this.layoutUtilsService.showActionNotification(
+              "Erreur lors de l'ajout de la population",
+              MessageType.Create,
+              10000,
+              true,
+              true
+            ),
+          () => {
+            this.createPopulationForm()
+            this.layoutUtilsService.showActionNotification(
+              "Promotion ajoutée avec succès",
+              MessageType.Create,
+              10000,
+              true,
+              true
+            );
+             //this.router.navigateByUrl("/auth/new-customer/");
+         }
+       );
   }
   
 
@@ -256,12 +288,14 @@ export class PromoAddComponent implements OnInit {
 
 
   // GRIDS 
+
+//  ******************* GRID POPULATION ARTICLE 
   open3(content) {
-    this.prepareGridRoleController()
+    this.preparePopsArticleGrid()
     this.modalService.open(content, { size: "lg" })
   }
 
-  prepareGridRoleController() {
+  preparePopsArticleGrid() {
     this.columnDefinitions3 = [
         {
             id: "id",
@@ -354,14 +388,14 @@ export class PromoAddComponent implements OnInit {
 }
 
 
-// ****************
+//  ******************* GRID SITES
 
 open8(content) {
   this.prepareGridSite()
   this.modalService.open(content, { size: "lg" })
-  }
+}
 
-  prepareGridSite() {
+prepareGridSite() {
     this.columnDefinitions6 = [
               {
                   id: "si_site",
@@ -414,27 +448,24 @@ open8(content) {
           }
         };
   
-  }
+}
 
-
-  gridReady6(angularGrid: AngularGridInstance) {
+gridReady6(angularGrid: AngularGridInstance) {
     this.angularGrid6 = angularGrid;
     this.dataView6 = angularGrid.dataView;
     this.gridObj6 = angularGrid.slickGrid;
     this.gridService6 = angularGrid.gridService;
-    }
+}
     
-    angularGridReady6(angularGrid: AngularGridInstance) {
+angularGridReady6(angularGrid: AngularGridInstance) {
       this.angularGrid6 = angularGrid;
       this.dataView6 = angularGrid.dataView;
       this.gridObj6 = angularGrid.slickGrid;
       this.gridService6 = angularGrid.gridService;
     
-    }
+}
     
-    
-    
-    handleSelectedSite(e, args) {
+handleSelectedSite(e, args) {
      const controls = this.populationForm.controls
      if(args.rows.length >0){
        let index = args.rows[0]
@@ -444,9 +475,9 @@ open8(content) {
        
      }
     
-    }
+}
 
-    getSites(){
+getSites(){
       this.promotionService.getAllSites().subscribe(
       
     (response: any) => {
@@ -458,6 +489,206 @@ open8(content) {
     },
     () => {}
     )
-    }
+}
+
+//  ******************* GRID POPULATION CLIENT
+open2(content) {
+  this.prepareGridPopClient()
+  this.modalService.open(content, { size: "lg" })
+}
+
+prepareGridPopClient() {
+    this.columnDefinitions2 = [
+      {
+        id: "id",
+        name: "id",
+        field: "id",
+        sortable: true,
+        minWidth: 80,
+        maxWidth: 80,
+    },
+    {
+        id: "population_code",
+        name: "Code",
+        field: "population_code",
+        sortable: true,
+        filterable: true,
+        type: FieldType.string,
+        },
+        {
+          id: "population_desc",
+          name: "Description",
+          field: "population_desc",
+          sortable: true,
+          filterable: true,
+          type: FieldType.string,
+        },   
+        ]
+  
+        this.gridOptions2 = {
+          asyncEditorLoading: false,
+          editable: false,
+          enableAddRow:true,
+          enableAutoResize:true,
+          autoHeight:true,
+          enableColumnPicker: true,
+          enableCellNavigation: true,
+          enableRowSelection: true,
+          enableCheckboxSelector: true,
+          rowSelectionOptions: {
+            selectActiveRow: false
+          }
+        };
+  
+}
+
+gridReady2(angularGrid: AngularGridInstance) {
+    this.angularGrid2 = angularGrid;
+    this.dataView2 = angularGrid.dataView;
+    this.gridObj2 = angularGrid.slickGrid;
+    this.gridService2 = angularGrid.gridService;
+}
+    
+angularGridReady2(angularGrid: AngularGridInstance) {
+      this.angularGrid2 = angularGrid;
+      this.dataView2 = angularGrid.dataView;
+      this.gridObj2 = angularGrid.slickGrid;
+      this.gridService2 = angularGrid.gridService;
+    
+}
+    
+handleSelectedPopClient(e, args) {
+     const controls = this.populationForm.controls
+     if(args.rows.length >0){
+       let index = args.rows[0]
+       const pop = this.gridObj2.getDataItem(index)
+    
+       controls.pop_c_code.setValue(pop.population_code)
+       
+     }
+    
+}
+
+getPopsClient(){
+      this.promotionService.getPopsClient().subscribe(
+      
+    (response: any) => {
+      this.popsClient = response.data
+      this.dataView2.setItems(this.popsClient)
+    },
+    (error) => {
+      this.popsClient = []
+    },
+    () => {}
+    )
+}
+
+//  ******************* GRID ADVANTAGES
+open4(content) {
+  this.prepareGridAdvantages()
+  this.modalService.open(content, { size: "lg" })
+}
+
+prepareGridAdvantages() {
+    this.columnDefinitions4 = [
+      {
+        id: "id",
+        name: "id",
+        field: "id",
+        sortable: true,
+        minWidth: 80,
+        maxWidth: 80,
+    },
+    {
+        id: "adv_code",
+        name: "Code",
+        field: "adv_code",
+        sortable: true,
+        filterable: true,
+        type: FieldType.string,
+        },
+        {
+          id: "adv_type",
+          name: "Type",
+          field: "adv_type",
+          sortable: true,
+          filterable: true,
+          type: FieldType.string,
+        },   
+        {
+          id: "adv_amount",
+          name: "Montant",
+          field: "adv_amount",
+          sortable: true,
+          filterable: true,
+          type: FieldType.string,
+        },  
+        {
+          id: "product_list",
+          name: "List produits",
+          field: "product_list",
+          sortable: true,
+          filterable: true,
+          type: FieldType.string,
+        },  
+        ]
+  
+        this.gridOptions4 = {
+          asyncEditorLoading: false,
+          editable: false,
+          enableAddRow:true,
+          enableAutoResize:true,
+          autoHeight:true,
+          enableColumnPicker: true,
+          enableCellNavigation: true,
+          enableRowSelection: true,
+          enableCheckboxSelector: true,
+          rowSelectionOptions: {
+            selectActiveRow: false
+          }
+        };
+  
+}
+
+gridReady4(angularGrid: AngularGridInstance) {
+    this.angularGrid4 = angularGrid;
+    this.dataView4 = angularGrid.dataView;
+    this.gridObj4 = angularGrid.slickGrid;
+    this.gridService4 = angularGrid.gridService;
+}
+    
+angularGridReady4(angularGrid: AngularGridInstance) {
+      this.angularGrid4 = angularGrid;
+      this.dataView4 = angularGrid.dataView;
+      this.gridObj4 = angularGrid.slickGrid;
+      this.gridService4 = angularGrid.gridService;
+    
+}
+    
+handleSelectedAdvantage(e, args) {
+     const controls = this.populationForm.controls
+     if(args.rows.length >0){
+       let index = args.rows[0]
+       const adv = this.gridObj4.getDataItem(index)
+    
+       controls.adv_code.setValue(adv.adv_code)
+       
+     }
+    
+}
+
+getAdvantages(){
+      this.promotionService.getAllAdvantages().subscribe(
+      
+    (response: any) => {
+      this.advantages = response.data
+      this.dataView2.setItems(this.advantages)
+    },
+    (error) => {
+      this.advantages = []
+    },
+    () => {}
+    )
+}
 
 }
