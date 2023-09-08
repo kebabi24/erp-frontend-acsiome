c5bd35e6370658600f030f5fd1f51c263144d7ea
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2023-09-08 14:32:52 +0100

    printrecept

--- src/app/views/pages/inventory-transaction/inventory-transaction.module.ts
+++ src/app/views/pages/inventory-transaction/inventory-transaction.module.ts
@@ -1,150 +1,120 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { InventoryTransactionComponent } from './inventory-transaction.component';
-import { TransferComponent } from './transfer/transfer.component';
-import { UnplanifiedIssueComponent } from './unplanified-issue/unplanified-issue.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { InventoryTransactionComponent } from "./inventory-transaction.component";
+import { TransferComponent } from "./transfer/transfer.component";
+import { UnplanifiedIssueComponent } from "./unplanified-issue/unplanified-issue.component";
 
-import { RouterModule, Routes } from "@angular/router"
-import { FormsModule, ReactiveFormsModule } from "@angular/forms"
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http"
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 // Fake API Angular-in-memory
-import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api"
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
 // Translate Module
-import { TranslateModule } from "@ngx-translate/core"
+import { TranslateModule } from "@ngx-translate/core";
 // NGRX
-import { StoreModule } from "@ngrx/store"
-import { EffectsModule } from "@ngrx/effects"
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
 // UI
-import { PartialsModule } from "../../partials/partials.module"
+import { PartialsModule } from "../../partials/partials.module";
 // Core
-import { FakeApiService } from "../../../core/_base/layout"
+import { FakeApiService } from "../../../core/_base/layout";
 // Auth
-import { ModuleGuard } from "../../../core/auth"
+import { ModuleGuard } from "../../../core/auth";
 
 // Core => Utils
-import {
-    HttpUtilsService,
-    TypesUtilsService,
-    InterceptService,
-    LayoutUtilsService,
-} from "../../../core/_base/crud"
+import { HttpUtilsService, TypesUtilsService, InterceptService, LayoutUtilsService } from "../../../core/_base/crud";
 // Shared
-import {
-    ActionNotificationComponent,
-    DeleteEntityDialogComponent,
-    FetchEntityDialogComponent,
-    UpdateStatusDialogComponent,
-} from "../../partials/content/crud"
+import { ActionNotificationComponent, DeleteEntityDialogComponent, FetchEntityDialogComponent, UpdateStatusDialogComponent } from "../../partials/content/crud";
 
 // Material
-import { MatMenuModule } from "@angular/material/menu"
-import { MatButtonModule } from '@angular/material/button';
-import { MatTabsModule } from "@angular/material/tabs"
+import { MatMenuModule } from "@angular/material/menu";
+import { MatButtonModule } from "@angular/material/button";
+import { MatTabsModule } from "@angular/material/tabs";
 //bootsrap
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
-import { environment } from "../../../../environments/environment"
-import {
-    NgbProgressbarModule,
-    NgbProgressbarConfig,
-} from "@ng-bootstrap/ng-bootstrap"
-import { NgxPermissionsModule } from "ngx-permissions"
-import { AngularSlickgridModule } from 'angular-slickgrid'
-import { UnplanifiedReceptComponent } from './unplanified-recept/unplanified-recept.component';
-import { InventoryListComponent } from './inventory-list/inventory-list.component';
-import { DomainService, LocationDetailService, SequenceService }  from '../../../core/erp';
-import { PoReceipComponent } from './po-receip/po-receip.component';
-import {PurchaseOrderService, 
-        TransferService,
-        PurchaseReceiveService,
-        InventoryTransactionService,
-        DeviseService,
-        ProviderService,
-        CostSimulationService,
-        SiteService,
-        LocationService,
-        InventoryStatusService,
-        RequisitionService,
-        MesureService,
-        UsersService,
-        ItemService,
-        LabelService,
-        } from '../../../core/erp';
-import { TransactionListComponent } from './transaction-list/transaction-list.component';
-import { EditStatusComponent } from './edit-status/edit-status.component';
-import { TransListGrpComponent } from './trans-list-grp/trans-list-grp.component';
-import { ListInvComponent } from './list-inv/list-inv.component';
-import { ListRctComponent } from './list-rct/list-rct.component';
-import { ConsoReportComponent } from './conso-report/conso-report.component';
-import { LabelCreateComponent } from './label-create/label-create.component';
-import { PoReceipIdComponent } from './po-receip-id/po-receip-id.component';
-import { PoReceipCabComponent } from './po-receip-cab/po-receip-cab.component';
-import { EditStatusRefComponent } from './edit-status-ref/edit-status-ref.component';
+import { environment } from "../../../../environments/environment";
+import { NgbProgressbarModule, NgbProgressbarConfig } from "@ng-bootstrap/ng-bootstrap";
+import { NgxPermissionsModule } from "ngx-permissions";
+import { AngularSlickgridModule } from "angular-slickgrid";
+import { UnplanifiedReceptComponent } from "./unplanified-recept/unplanified-recept.component";
+import { InventoryListComponent } from "./inventory-list/inventory-list.component";
+import { DomainService, LocationDetailService, SequenceService } from "../../../core/erp";
+import { PoReceipComponent } from "./po-receip/po-receip.component";
+import { PurchaseOrderService, TransferService, PurchaseReceiveService, InventoryTransactionService, DeviseService, ProviderService, CostSimulationService, SiteService, LocationService, InventoryStatusService, RequisitionService, MesureService, UsersService, ItemService, LabelService, PrintersService } from "../../../core/erp";
+import { TransactionListComponent } from "./transaction-list/transaction-list.component";
+import { EditStatusComponent } from "./edit-status/edit-status.component";
+import { TransListGrpComponent } from "./trans-list-grp/trans-list-grp.component";
+import { ListInvComponent } from "./list-inv/list-inv.component";
+import { ListRctComponent } from "./list-rct/list-rct.component";
+import { ConsoReportComponent } from "./conso-report/conso-report.component";
+import { LabelCreateComponent } from "./label-create/label-create.component";
+import { PoReceipIdComponent } from "./po-receip-id/po-receip-id.component";
+import { PoReceipCabComponent } from "./po-receip-cab/po-receip-cab.component";
+import { EditStatusRefComponent } from "./edit-status-ref/edit-status-ref.component";
 
 const routes: Routes = [
   {
-    path: 'transfer',
-    component: TransferComponent
+    path: "transfer",
+    component: TransferComponent,
   },
   {
-    path: 'unplanified-issue',
-    component: UnplanifiedIssueComponent
+    path: "unplanified-issue",
+    component: UnplanifiedIssueComponent,
   },
   {
-    path: 'unplanified-recept',
-    component: UnplanifiedReceptComponent
+    path: "unplanified-recept",
+    component: UnplanifiedReceptComponent,
   },
   {
-    path: 'inventory-list',
-    component: InventoryListComponent
+    path: "inventory-list",
+    component: InventoryListComponent,
   },
   {
-    path: 'transaction-list',
-    component: TransactionListComponent
+    path: "transaction-list",
+    component: TransactionListComponent,
   },
   {
-    path: 'conso-report',
-    component: ConsoReportComponent
+    path: "conso-report",
+    component: ConsoReportComponent,
   },
   {
-    path: 'trans-list-grp',
-    component: TransListGrpComponent
+    path: "trans-list-grp",
+    component: TransListGrpComponent,
   },
   {
-    path: 'po-receip',
-    component: PoReceipComponent
+    path: "po-receip",
+    component: PoReceipComponent,
   },
   {
-    path: 'edit-status',
-    component: EditStatusComponent
+    path: "edit-status",
+    component: EditStatusComponent,
   },
   {
-    path: 'edit-status-ref',
-    component: EditStatusRefComponent
+    path: "edit-status-ref",
+    component: EditStatusRefComponent,
   },
   {
-    path: 'list-inv',
-    component: ListInvComponent
+    path: "list-inv",
+    component: ListInvComponent,
   },
   {
-    path: 'list-rct',
-    component: ListRctComponent
+    path: "list-rct",
+    component: ListRctComponent,
   },
   {
-    path: 'label-create',
-    component: LabelCreateComponent
+    path: "label-create",
+    component: LabelCreateComponent,
   },
   {
     path: "po-receip-id/:id",
     component: PoReceipIdComponent,
   },
   {
-    path: 'po-receip-cab',
-    component: PoReceipCabComponent
+    path: "po-receip-cab",
+    component: PoReceipCabComponent,
   },
-] 
-
-
+];
 
 @NgModule({
   declarations: [InventoryTransactionComponent, TransferComponent, UnplanifiedIssueComponent, UnplanifiedReceptComponent, InventoryListComponent, PoReceipComponent, TransactionListComponent, EditStatusComponent, TransListGrpComponent, ListInvComponent, ListRctComponent, ConsoReportComponent, LabelCreateComponent, PoReceipIdComponent, PoReceipCabComponent, EditStatusRefComponent],
@@ -163,48 +133,48 @@ const routes: Routes = [
     MatButtonModule,
     NgbModule,
     environment.isMockEnabled
-        ? HttpClientInMemoryWebApiModule.forFeature(FakeApiService, {
-              passThruUnknownUrl: true,
-              dataEncapsulation: false,
-          })
-        : [],
-],
+      ? HttpClientInMemoryWebApiModule.forFeature(FakeApiService, {
+          passThruUnknownUrl: true,
+          dataEncapsulation: false,
+        })
+      : [],
+  ],
 
+  providers: [
+    ModuleGuard,
+    InterceptService,
 
-providers: [
-  ModuleGuard,
-  InterceptService,
-  
-  {
+    {
       provide: HTTP_INTERCEPTORS,
       useClass: InterceptService,
       multi: true,
-  },
+    },
 
-  TypesUtilsService,
-  LayoutUtilsService,
-  HttpUtilsService,
-  TransferService,
-  TypesUtilsService,
-  LayoutUtilsService,
-  LocationDetailService,
-  PurchaseOrderService,
-  InventoryTransactionService,
-  PurchaseReceiveService,
-  DeviseService,
-  ProviderService,
-  CostSimulationService,
-  ItemService,
-  SiteService,
-  LocationService,
-  MesureService,
-  SequenceService,
-  LocationDetailService,
-  InventoryStatusService,
-  RequisitionService,
-  UsersService,
-  LabelService,
-  DomainService
-],
+    TypesUtilsService,
+    LayoutUtilsService,
+    HttpUtilsService,
+    TransferService,
+    TypesUtilsService,
+    LayoutUtilsService,
+    LocationDetailService,
+    PurchaseOrderService,
+    InventoryTransactionService,
+    PurchaseReceiveService,
+    DeviseService,
+    ProviderService,
+    CostSimulationService,
+    ItemService,
+    SiteService,
+    LocationService,
+    MesureService,
+    SequenceService,
+    LocationDetailService,
+    InventoryStatusService,
+    RequisitionService,
+    UsersService,
+    LabelService,
+    DomainService,
+    PrintersService,
+  ],
 })
-export class InventoryTransactionModule { }
+export class InventoryTransactionModule {}
