fc8cb6ea147108a1439774db24c374d94fc245bb
abdelhakbenbouzid <69382838+abdelhakbenbouzid@users.noreply.github.com>
2024-02-12 11:16:40 +0100

    METIER ET MAITRISE

--- src/app/views/pages/project/add-report/add-report.component.ts
+++ src/app/views/pages/project/add-report/add-report.component.ts
@@ -1269,19 +1269,31 @@ export class AddReportComponent implements OnInit {
       args.rows.map((idx) => {
         const item = this.gridObjemp.getDataItem(idx);
         console.log(item);
-        // if (item.emp_job != this.job || item.emp_level != this.level) {
-        //   alert("Métier ou Niveai de maitrise ne correspond pas a cet employé");
-        //   updateItem.pmr_employe = null;
-        //   this.mvgridService.updateItem(updateItem);
-        // } else {
-        updateItem.pmr_employe = item.emp_addr;
-        updateItem.fname = item.emp_fname;
-        updateItem.lname = item.emp_lname;
-        updateItem.job = item.emp_job;
-        updateItem.level = item.emp_level;
-
-        this.mvgridService.updateItem(updateItem);
-        // }
+        this.employeService
+          .getByJob({empj_addr: item.emp_addr, empj_job: this.job })
+          .subscribe((response: any) => {
+            if (response.data.length == 0) 
+         
+            {         
+                 
+                 
+                  alert("Métier demandé ne correspond pas a cet employé")
+                  updateItem.pme_employe = null
+                  this.mvgridService.updateItem(updateItem)
+                } else {   
+                  if (Number(response.data[0].empj_level) < Number(this.level)){alert("niveau de maitrise demandée ne correspond pas a cet employé")
+                  updateItem.pme_employe = null
+                  this.mvgridService.updateItem(updateItem)}
+                  else{
+                      updateItem.pme_employe = item.emp_addr
+                      updateItem.fname = item.emp_fname
+                      updateItem.lname = item.emp_lname
+                      updateItem.job = response.data[0].empj_job
+                      updateItem.level = response.data[0].empj_level
+                      this.mvgridService.updateItem(updateItem)
+                    }
+                 }
+        })
       });
     }
   }
