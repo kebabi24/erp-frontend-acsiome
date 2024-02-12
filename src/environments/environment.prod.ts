cb1ae348c1b83f24b346cf39e51d897a6832f480
Kebabi Abdelmounaim <kebabiabdel@gmail.com>
2024-02-12 12:54:36 +0100

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,9 +3,9 @@ export const environment = {
   isMockEnabled: true, // You have to switch this, when your real back-end is done
   authTokenKey: "authce9d77b308c149d5992a80073637e4d5",
   // apiUrl: "http://localhost:3001/api/v1",
- 
+  apiUrl: "http://10.10.3.8:3236/api/v1",
   // apiUrl: "http://192.168.1.253:3001/api/v1",
-  apiUrl: "http://localhost:3001/api/v1",
-  Instance : "Prod"
+  // apiUrl: "http://localhost:3001/api/v1",
+  Instance: "Prod",
   //willda apiUrl: "http://192.168.1.252:3001/api/v1",
 };
