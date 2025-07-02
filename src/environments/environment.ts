b9b06c0111116870197d6b742d5494586acd27c2
abdelhakbenbouzid <69382838+abdelhakbenbouzid@users.noreply.github.com>
2025-07-02 22:58:22 +0100

    020725

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,22 +6,21 @@ export const environment = {
   production: false,
   isMockEnabled: true, // You have to switch this, when your real back-end is done
   authTokenKey: "authce9d77b308c149d5992a80073637e4d5",
-    apiUrl: "http://localhost:3001/api/v1",
-    // apiUrl: "http://192.168.60.52:3001/api/v1",
+  apiUrl: "http://localhost:3001/api/v1",
+  // apiUrl: "http://192.168.60.52:3001/api/v1",
   //  apiUrl: "http://192.168.1.252:3001/api/v1", //WILLDA
   //  apiUrl: "http://192.168.2.1:3001/api/v1", //TARCHID
   //  apiUrl: "http://105.98.96.17:3001/api/v1",
   //  apiUrl: "http://10.10.130.9:3001/api/v1", //EDELWEISS
   //  apiUrl: "http://10.10.3.6:4341/api/v1", //PRIMA
-    // apiUrl: "https://10.10.12.90:3001/api/v1", //PALMARY
+  // apiUrl: "https://10.10.12.90:3001/api/v1", //PALMARY
   //  apiUrl: "http://192.168.100.100:3001/api/v1", 
   // apiUrl: "http://192.168.1.20:3001/api/v1",
   // apiUrl: "http://25.7.193.22:3001/api/v1",
-    // apiUrl: "https://edelweiss.whitebay.limited/api/v1",
+  // apiUrl: "https://edelweiss.whitebay.limited/api/v1",
   Instance: "Prod",
-  
   App: "ERP", // RH or ERP or DD
-  key:"$argon2id$v=19$m=4096,t=3,p=1$MTIzNDU2Nzg$hoqzObkMeMlG9nOUKy0g8xwyih/9gjPFUvgRJ229lwQ",
+  key:"$argon2i$v=19$m=16,t=2,p=1$MTIzNDU2Nzg$b7dhzjxkuGRbLPCGSXW3Yw",
 };
 
 /*
