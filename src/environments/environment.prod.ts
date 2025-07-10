105359a4ba1b03aa05cf4ec10fc8fea4c3f90870
abdelhakbenbouzid <69382838+abdelhakbenbouzid@users.noreply.github.com>
2025-07-10 10:22:37 +0100

    100725

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -24,6 +24,6 @@ export const environment = {
   App: "ERP",
    
 
-  key:"$argon2id$v=19$m=4096,t=3,p=1$MTIzNDU2Nzg$hoqzObkMeMlG9nOUKy0g8xwyih/9gjPFUvgRJ229lwQ",
+  key:"$argon2i$v=19$m=16,t=2,p=1$MTIzNDU2Nzg$b7dhzjxkuGRbLPCGSXW3Yw",
  //edelweiss key:"$argon2id$v=19$m=4096,t=3,p=1$MTIzNDU2Nzg$zHjTc5y+XDtK0x6zun1KfIkkfhpDa+rYV4lBLLZX4Bc"
 };
